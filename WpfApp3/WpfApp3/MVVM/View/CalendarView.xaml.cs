74f34c34faf8f01da960efbff6e9837f473366f2
jussi hakkarainen <jussi.p.hakkarainen@gmail.com>
2023-10-04 11:49:26 +0300

    Added dates to each day of the week in calender view

--- WpfApp3/WpfApp3/MVVM/View/CalendarView.xaml.cs
+++ WpfApp3/WpfApp3/MVVM/View/CalendarView.xaml.cs
@@ -24,13 +24,43 @@ namespace WpfApp3.MVVM.View
         List<UserData> userDataList = new List<UserData>();
 
         string username = "Jussi";
-
+        DateTime currentDate = DateTime.Now;
 
         public CalendarView()
         {
             InitializeComponent();
+            DateTime mondayDate = GetMondayOfCurrentWeek(DateTime.Now);
+            mondayDateTextBox.Text = mondayDate.ToString("dd/MM");
+
+            DateTime tuesdayDate = mondayDate.AddDays(1);
+            tuesdayDateTextBox.Text = tuesdayDate.ToString("dd/MM");
+
+            DateTime wednesdayDate = mondayDate.AddDays(2);
+            wednesdayDateTextBox.Text = wednesdayDate.ToString("dd/MM");
+
+            DateTime thursdayDate = mondayDate.AddDays(3);
+            thursdayDateTextBox.Text = thursdayDate.ToString("dd/MM");
+
+            DateTime fridayDate = mondayDate.AddDays(4);
+            fridayDateTextBox.Text = fridayDate.ToString("dd/MM");
+
+            DateTime saturdayDate = mondayDate.AddDays(5);
+            saturdayDateTextBox.Text = saturdayDate.ToString("dd/MM");
+
+            DateTime sundayDate = mondayDate.AddDays(6);
+            sundayDateTextBox.Text = sundayDate.ToString("dd/MM");
+
+
         }
-                
+        private DateTime GetMondayOfCurrentWeek(DateTime currentDate)
+        {
+            int daysUntilMonday = ((int)currentDate.DayOfWeek - (int)DayOfWeek.Monday + 7) % 7;
+            DateTime mondayOfWeek = currentDate.AddDays(-daysUntilMonday);
+            return mondayOfWeek;
+        }
+
+
+
         private void TextBlock_TextInput(object sender, TextCompositionEventArgs e)
         {
 
