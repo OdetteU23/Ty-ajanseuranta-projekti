fd5fefad492ef009cf68d7ba022809c5bb4e88f9
jussi hakkarainen <jussi.p.hakkarainen@gmail.com>
2023-10-06 08:49:35 +0300

    fixed next and previous button on calender

--- WpfApp3/WpfApp3/MVVM/View/CalendarView.xaml.cs
+++ WpfApp3/WpfApp3/MVVM/View/CalendarView.xaml.cs
@@ -25,14 +25,14 @@ namespace WpfApp3.MVVM.View
         List<UserData> userDataList = new List<UserData>();
 
         string username = "Jussi";
-        DateTime mondayDate;
-        DateTime tuesdayDate;
-        DateTime wednesdayDate;
-        DateTime thursdayDate;
-        DateTime fridayDate;
-        DateTime saturdayDate;
-        DateTime sundayDate;
-        
+        private DateTime mondayDate;
+        private DateTime tuesdayDate;
+        private DateTime wednesdayDate;
+        private DateTime thursdayDate;
+        private DateTime fridayDate;
+        private DateTime saturdayDate;
+        private DateTime sundayDate;
+
 
 
 
@@ -40,25 +40,25 @@ namespace WpfApp3.MVVM.View
         {
             InitializeComponent();
 
-            DateTime mondayDate = GetMondayOfCurrentWeek(DateTime.Now);
+            mondayDate = GetMondayOfCurrentWeek(DateTime.Now);
             mondayDateTextBox.Text = mondayDate.ToString("dd/MM");
 
-            DateTime tuesdayDate = mondayDate.AddDays(1);
+            tuesdayDate = mondayDate.AddDays(1);
             tuesdayDateTextBox.Text = tuesdayDate.ToString("dd/MM");
 
-            DateTime wednesdayDate = mondayDate.AddDays(2);
+            wednesdayDate = mondayDate.AddDays(2);
             wednesdayDateTextBox.Text = wednesdayDate.ToString("dd/MM");
 
-            DateTime thursdayDate = mondayDate.AddDays(3);
+            thursdayDate = mondayDate.AddDays(3);
             thursdayDateTextBox.Text = thursdayDate.ToString("dd/MM");
 
-            DateTime fridayDate = mondayDate.AddDays(4);
+            fridayDate = mondayDate.AddDays(4);
             fridayDateTextBox.Text = fridayDate.ToString("dd/MM");
 
-            DateTime saturdayDate = mondayDate.AddDays(5);
+            saturdayDate = mondayDate.AddDays(5);
             saturdayDateTextBox.Text = saturdayDate.ToString("dd/MM");
 
-            DateTime sundayDate = mondayDate.AddDays(6);
+            sundayDate = mondayDate.AddDays(6);
             sundayDateTextBox.Text = sundayDate.ToString("dd/MM");
 
 
@@ -110,9 +110,24 @@ namespace WpfApp3.MVVM.View
 
         void OnButtonPrevious_Click(object sender, RoutedEventArgs e)
         {
-            
-          
-           
+            mondayDate = mondayDate.AddDays(-7);
+            tuesdayDate = tuesdayDate.AddDays(-7);
+            wednesdayDate = wednesdayDate.AddDays(-7);
+            thursdayDate = thursdayDate.AddDays(-7);
+            fridayDate = fridayDate.AddDays(-7);
+            saturdayDate = saturdayDate.AddDays(-7);
+            sundayDate = sundayDate.AddDays(-7);
+
+            // päiviteteen teksti kentät
+            mondayDateTextBox.Text = mondayDate.ToString("dd/MM");
+            tuesdayDateTextBox.Text = tuesdayDate.ToString("dd/MM");
+            wednesdayDateTextBox.Text = wednesdayDate.ToString("dd/MM");
+            thursdayDateTextBox.Text = thursdayDate.ToString("dd/MM");
+            fridayDateTextBox.Text = fridayDate.ToString("dd/MM");
+            saturdayDateTextBox.Text = saturdayDate.ToString("dd/MM");
+            sundayDateTextBox.Text = sundayDate.ToString("dd/MM");
+
+
         }
 
         void OnButtonShow_Click(object sender, RoutedEventArgs e)
