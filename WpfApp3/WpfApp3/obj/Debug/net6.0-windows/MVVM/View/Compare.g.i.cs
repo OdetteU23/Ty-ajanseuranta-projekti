ebd4d4dc52c96162ad9e87620e7b954ad6ea9f41
Kati Lehtonen <125460153+Dezaiasu@users.noreply.github.com>
2023-10-10 15:15:10 +0300

    Piti vetää projekti uudestaan koneelle ja uudelleen kirjottaa työaikoja.

--- WpfApp3/WpfApp3/obj/Debug/net6.0-windows/MVVM/View/Compare.g.i.cs
+++ WpfApp3/WpfApp3/obj/Debug/net6.0-windows/MVVM/View/Compare.g.i.cs
@@ -111,13 +111,13 @@ namespace WpfApp3.MVVM.View {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp3;component/mvvm/view/compare.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp3;V1.0.0.0;component/mvvm/view/compare.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\View\Compare.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -127,7 +127,7 @@ namespace WpfApp3.MVVM.View {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
--- WpfApp3/WpfApp3/obj/Debug/net6.0-windows/StartWindow.g.i.cs
+++ WpfApp3/WpfApp3/obj/Debug/net6.0-windows/StartWindow.g.i.cs
@@ -48,13 +48,13 @@ namespace WpfApp3 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp3;component/startwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp3;V1.0.0.0;component/startwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\StartWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -64,7 +64,7 @@ namespace WpfApp3 {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
